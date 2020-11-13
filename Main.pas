211ea322eb15a66267c7b5e7717297d2230ede32
Dave Blakeman <dave.blakeman@rpssoftware.co.uk>
2020-11-13 20:03:21 +0000

    Fix: Remove ref to unused RzTabs unit

--- Main.pas
+++ Main.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls, RzTabs,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Menus, Vcl.StdCtrls,
   DelphiProject, LexicalAnalyser, Vcl.Grids,
   DelphiUnit, Vcl.ComCtrls, Vcl.ExtCtrls, System.UITypes;
 
